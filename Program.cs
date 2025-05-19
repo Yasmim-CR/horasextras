4d52fdadd4c0cf2c20d20bfd3028c9dd8990d120
LAB53 <lab53@fatec.com>
2025-05-19 08:00:13 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int horasTrabalhadas, horasExtras;
+decimal valorSalario;
+
+Console.WriteLine("Olá trabalhador, vamos calcular seu salário!");
+Console.WriteLine("Pressione qualquer tecla para continuar...");
+Console.ReadKey();
+
+Console.Write("Digite o valor do salário: R$ ");
+valorSalario = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.Write("Digite as horas trabalhadas: ");
+horasTrabalhadas = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Digite as horas extras trabalhadas: ");
+horasExtras = Convert.ToInt32(Console.ReadLine()!);
+
+
+Console.WriteLine($"O salário total é: {CalculaSalario():C2}");
+
+decimal CalculaSalario()
+{
+    decimal salario = valorSalario * horasTrabalhadas;
+    decimal salarioTotal = salario + (horasExtras * 1.4m);
+    return salarioTotal;
+}
